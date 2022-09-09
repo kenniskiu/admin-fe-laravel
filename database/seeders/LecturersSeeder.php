f1ad8bb14b22bf592c0b28be7f41a89f8d52bad7
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-09-09 14:33:05 +0700

    add migrate, model, seeder

---
+++ database/seeders/LecturersSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class LecturersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('lecturers')->insert([
+            [
+                'name' => "ken",
+                'is_lecturer'  => true,
+                'is_mentor'  => false,
+                'created_at' => Carbon::now(),
+            ],
+            [
+                'name' => "lukas",
+                'is_lecturer'  => false,
+                'is_mentor'  => false,
+                'created_at' => Carbon::now(),
+            ]
+        ]);
+    }
+}
